2288799a6ae491ed3c6a65852390a27bb9a358c3
Joel Eche <joel.eche5@gmail.com>
2017-09-08 00:21:05 -0500

    Counting for non-existing words

--- seccion8/application1/app1.py
+++ seccion8/application1/app1.py
@@ -3,7 +3,10 @@ import json
 data=json.load(open('data.json'))
 
 def traslate(w):
-    return data[w]
+    if w in data:
+        return data[w]
+    else:
+        return 'The word doesn\'t exist. Please check it.'
 
 word=input('Enter word: ')
 
