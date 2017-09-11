2ed247be138fa59be406108b5c60c017cfe9b082
Joel Eche <joel.eche5@gmail.com>
2017-09-11 18:14:33 -0500

    Prompting the user to confirm similarity check

--- seccion8/application1/app1.py
+++ seccion8/application1/app1.py
@@ -8,7 +8,13 @@ def traslate(w):
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) > 0:
-        return 'Did you mean %s instead?' % get_close_matches(w, data.keys())[0]
+        yn= input('Did you mean %s instead? Enter Y if yes, or N if no.' % get_close_matches(w, data.keys())[0])
+        if yn.lower()=='y':
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn.lower()=='n':
+            return 'The word doesn\'t exist. Please check it.'
+        else:
+            return 'We didn\'t understand you entry.'
     else:
         return 'The word doesn\'t exist. Please check it.'
 
